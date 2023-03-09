07f2badd1108fdcaece7304327e64e2c378a71ca
HellHorseman <duke.sheffer@yandex.ru>
2023-03-09 23:53:00 +0400

    Final review

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,18 @@ public class Main {
     }
     public static void task2 () {
         System.out.println("Задача 2");
-        String fullName = "Ivanov Ivan Ivanovich";
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
     }
     public static void task3 () {
         System.out.println("Задача 3");
-        String fullName = "Иванов Семён Семёнович";
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
         fullName = fullName.replace("ё", "е");
         System.out.println("Данные ФИО сотрудника " + fullName);
     }
