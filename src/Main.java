021a4bc010ac4e7e0efa4384c392aece0eb493e5
HellHorseman <duke.sheffer@yandex.ru>
2023-03-09 23:54:32 +0400

    Final review

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
         task2();
         task3();
     }
-    public static void task1 () {
+
+    public static void task1() {
         System.out.println("Задача 1");
         String firstName = "Ivan";
         String middleName = "Ivanovich";
@@ -12,7 +13,8 @@ public class Main {
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника " + fullName);
     }
-    public static void task2 () {
+
+    public static void task2() {
         System.out.println("Задача 2");
         String firstName = "Ivan";
         String middleName = "Ivanovich";
@@ -20,7 +22,8 @@ public class Main {
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
     }
-    public static void task3 () {
+
+    public static void task3() {
         System.out.println("Задача 3");
         String firstName = "Семён";
         String middleName = "Семёнович";
